42fe6046b77e71476b2cad53af9dcd8de40533d7
Matheusbzrr <154834294+Matheusbzrr@users.noreply.github.com>
2025-09-11 14:18:01 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',              // quando você roda o front local
+      'https://seu-frontend.onrender.com',  // domínio do front em produção
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
